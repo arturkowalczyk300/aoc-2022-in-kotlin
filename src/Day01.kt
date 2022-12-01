c027af932fee6a5cc7e36b22c5464233befcff52
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2022-12-01 18:49:54 +0100

    add code solving puzzles from day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = 0
+        var currentCalories = 0
+        input.forEach {
+            if (it != "")
+                currentCalories += it.toIntOrNull() ?: 0
+            else {
+                if (currentCalories > maxCalories)
+                    maxCalories = currentCalories
+                currentCalories = 0
+            }
+        }
+
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val sumOfCalories = mutableListOf<Int>()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        var currentCalories = 0
+        input.forEach {
+            if (it != "")
+                currentCalories += it.toIntOrNull() ?: 0
+            else {
+                sumOfCalories.add(currentCalories)
+                currentCalories = 0
+            }
+        }
+
+        sumOfCalories.sortDescending()
+
+        return ((sumOfCalories.getOrNull(0) ?: 0)
+                + (sumOfCalories.getOrNull(1) ?: 0)
+                + (sumOfCalories.getOrNull(2) ?: 0))
+    }
 
     val input = readInput("Day01")
     println(part1(input))
