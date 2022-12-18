fecaacdbcbf5e5ee5cdc3a5204f1eec5cc51946d
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2022-12-18 23:35:11 +0100

    day 07: add code solving part01

--- src/Day07.kt
+++ src/Day07.kt
@@ -8,55 +8,54 @@ enum class PROMPT_TYPE {
 }
 
 abstract class FileSystemElement(var size: Int, var nodeName: String, var parent: FileSystemElement?) {
-    val children: MutableList<FileSystemElement> = mutableListOf()
+    protected val children: MutableList<FileSystemElement> = mutableListOf()
 
-    abstract fun calculateSizeOfChildrenDirectoriesWithMaxSize(maxsize: Int): Int
+    fun calculateSize(): Int {
+        var sum = 0
+        children.forEach {
+            sum += it.calculateSize()
+        }
+        if (children.isEmpty())
+            sum += this.size
+
+        this.size = sum
+
+        return sum
+    }
+
+    fun getChildrenReadOnly(): List<FileSystemElement> = children.toList()
+
+    fun addFile(file: File) {
+        children.add(file)
+        size += file.size
+    }
+
+    fun addDirectory(dir: Directory) {
+        children.add(dir)
+    }
+
+    fun getFlatten(): List<FileSystemElement> {
+        val mutableList = mutableListOf<FileSystemElement>()
+        children.forEach {
+            if (it is Directory)
+                mutableList.addAll(it.getFlatten())
+            mutableList.add(it)
+        }
+        return mutableList.toList()
+    }
 }
 
 
 class Root() : FileSystemElement(0, "/", null) {
-    override fun calculateSizeOfChildrenDirectoriesWithMaxSize(maxsize: Int): Int {
-        var sum = 0
-        children.forEach {
-            if (it.children.isNotEmpty()) //skip files
-            {
-                sum += it.calculateSizeOfChildrenDirectoriesWithMaxSize(maxsize)
-//                println("keysum=$sum")
-            }
-        }
-        return sum.also { println("root; return sum=$sum") }
-    }
+
 }
 
 class File(size: Int, nodeName: String, parent: FileSystemElement?) : FileSystemElement(size, nodeName, parent) {
-    override fun calculateSizeOfChildrenDirectoriesWithMaxSize(maxsize: Int): Int =
-        this.size.also { println("file; return sum=${this.size}") }
+
 }
 
 class Directory(size: Int, nodeName: String, parent: FileSystemElement?) : FileSystemElement(size, nodeName, parent) {
-    override fun calculateSizeOfChildrenDirectoriesWithMaxSize(maxsize: Int): Int {
-        var totalSum = 0
-        var sumOfFiles =0
-        var sumOfChildrenDirectories = 0
-        children.forEach {
-            totalSum += (it.calculateSizeOfChildrenDirectoriesWithMaxSize(maxsize) *
-                    (if (it.children.isNotEmpty()) 2 else 1)) // if current entity is directory, add twice!
-        }
-        return (if (totalSum <= maxsize) totalSum else 0).also { println("directory; return sum=$it") }
-    }
 
-
-//OLD:    override fun calculateSizeOfChildrenDirectoriesWithMaxSize(maxsize: Int): Int {
-//        var totalSum = 0
-//        var sumOfFiles =0
-//        var sumOfChildrenDirectories = 0
-//
-//        children.forEach {
-//            totalSum += (it.calculateSizeOfChildrenDirectoriesWithMaxSize(maxsize) *
-//                    (if (it.children.isNotEmpty()) 2 else 1)) // if current entity is directory, add twice!
-//        }
-//        return (if (totalSum <= maxsize) totalSum else 0).also { println("directory; return sum=$it") }
-//    }
 }
 
 class PromptCommand(
@@ -118,6 +117,8 @@ fun main() {
     //endregion
 
     //region file system checker (iterate and collect information about it)
+
+
     fun createFileStructureMap(listOfCommands: List<PromptCommand>): MutableList<FileSystemElement> {
         val fileSystemStructure: MutableList<FileSystemElement> = mutableListOf()
 
@@ -138,10 +139,10 @@ fun main() {
                 PROMPT_TYPE.COMMAND_CD_LOWER_LEVEL -> {
                     val prevCurrentElement = currentElement
                     val name = prompt.additionalInfo!![0]
-                    currentElement = currentElement!!.children.find { it.nodeName == name }
+                    currentElement = currentElement!!.getChildrenReadOnly().find { it.nodeName == name }
                     if (currentElement == null) {
                         val el = Directory(0, name, prevCurrentElement)
-                        prevCurrentElement!!.children.add(el)
+                        prevCurrentElement!!.addDirectory(el)
                         currentElement = el
                     }
                 }
@@ -154,10 +155,10 @@ fun main() {
                             val size = grp[0].toInt()
                             val name = grp[1]
                             //println("fullString=${nodeString}, size=${size}, name=${name}")
-                            currentElement!!.children.add(File(size, name, currentElement))
+                            currentElement!!.addFile(File(size, name, currentElement))
                         } else {//directory
                             val name = nodeString.split(" ")[1]
-                            currentElement!!.children.add(Directory(0, name, currentElement))
+                            currentElement!!.addDirectory(Directory(0, name, currentElement))
                         }
                     }
                 }
@@ -170,69 +171,20 @@ fun main() {
 
         return fileSystemStructure
     }
-
-
-    //endregion
-
-    //region handle map of files
-    /*this block of code needs input in format like:
-    - / (dir)
-    - a (dir)
-    - e (dir)
-    - i (file, size=584)
-    - f (file, size=29116)
-    - g (file, size=2557)
-    - h.lst (file, size=62596)
-    - b.txt (file, size=14848514)
-    - c.dat (file, size=8504156)
-    - d (dir)
-    - j (file, size=4060174)
-    - d.log (file, size=8033020)
-    - d.ext (file, size=5626152)
-    - k (file, size=7214296)*/
-
-
-    fun handleMapOfFiles(input: List<String>): Int {
-        val fsElements: MutableList<FileSystemElement> = mutableListOf()
-        fsElements.add(Root())
-        var currentElement: FileSystemElement = fsElements[0]
-        var indentationIndex = 0
-        var previousIndentationIndex = -1
-
-        //remove first line - it is always root
-        val inputWithoutFirstLine = input.drop(1)
-
-        inputWithoutFirstLine.forEach {
-            val size: Int = ("""([\d]+)""".toRegex().find(it)?.groupValues?.get(0)?.toInt()) ?: 0
-            val nextElement = if (it.contains("dir")) Directory(size, "dir", null) else File(size, "file", null)
-
-            indentationIndex = it.indexOf("-")
-
-            if (indentationIndex > previousIndentationIndex) {
-                nextElement.parent = currentElement
-                if (currentElement.children.size == 0)
-                    currentElement.children.add(nextElement)
-
-            } else if (indentationIndex == previousIndentationIndex) {
-                nextElement.parent = currentElement.parent
-                nextElement.parent?.children?.add(nextElement)
-            } else {
-                nextElement.parent = currentElement.parent!!.parent
-                nextElement.parent?.children?.add(nextElement)
-            }
-            currentElement = nextElement
-
-            previousIndentationIndex = indentationIndex
-        }
-
-        return fsElements[0].calculateSizeOfChildrenDirectoriesWithMaxSize(100000)
-    }
     //endregion
 
     fun part1(input: List<String>): Int {
         val commandsList = createListOfCommands(input)
         val fileStructure = createFileStructureMap(commandsList)
-        val size = fileStructure[0].calculateSizeOfChildrenDirectoriesWithMaxSize(100000)
+
+        val flatten = fileStructure[0].getFlatten()
+
+        fileStructure[0].calculateSize()
+        var size = flatten.sumOf {
+            if (it is Directory && it.size < 100000) it.size
+            else 0
+        }
+
         return size
     }
 
@@ -241,9 +193,9 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-   // val testInput = readInput("Day07_test")
-   // val sz =  part1(testInput).also { println("sz=${it}") }
-   // check(sz == 95437)
+    val testInput = readInput("Day07_test")
+    val sz = part1(testInput).also { println("sz=${it}") }
+    check(sz == 95437)
 
     val input = readInput("Day07")
     println(part1(input))
